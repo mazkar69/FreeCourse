d156e35d19391a5c894d73ede66437eccf4f2199
AzKaR <mohdazkar@outlook.com>
2022-07-12 19:29:31 +0530

    all section done with responsiveness

--- static/js/index.js
+++ static/js/index.js
@@ -6,3 +6,20 @@ dropdown.addEventListener("click",()=>{
     // console.log("clicked")
     dropdown_contant.classList.toggle("hdrop")
 })
+//Not working this function.
+dropdown.addEventListener("blur",()=>{
+    console.log("blur event fired");
+    dropdown_contant.classList.remove("hdrop");
+})
+
+
+
+const header = document.querySelector(".header");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click",()=>{
+    header.classList.toggle("active");
+})
+
+
+
